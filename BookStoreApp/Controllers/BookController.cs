592d26262dcbe440d7d324a8dcaf679e05dd0f9e
Chandan Kumar <ck801325@gmail.com>
2021-04-18 07:49:27 +0530

    Created Book store app

---
+++ BookStoreApp/Controllers/BookController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreApp.Controllers
+{
+    public class BookController : Controller
+    {
+        public string GetAllBooks()
+        {
+            return "All Books";
+        }
+        public string GetBook(int id)
+        {
+            return $"Id of the book is {id}";
+        }
+        public string SearchBooks(string bookName,string authorName)
+        {
+            return $"Book name is {bookName} and Author name is {authorName}";
+        }
+    }
+}
